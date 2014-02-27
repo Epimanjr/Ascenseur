8e1b1968f0dfaabf6d7b72022c2887a8f2b72812
Tixinoo <antoine.nosal@gmail.com>
2014-02-27 09:48:34 +0100

    // Tixinoo

--- src/MonAscenseur/EvenementOuverture.java
+++ src/MonAscenseur/EvenementOuverture.java
@@ -16,6 +16,14 @@ public class EvenementOuverture extends Evenement {
         super(d);
     }
 
+/*
+Se produit au moment très précis où les portes de la cabine viennent juste de terminer leur ouverture.
+Cet événement est généré par le traitement d’un événement PCP quand la cabine doit s’arrêter.
+Il faut 5 dixièmes de seconde pour arrêter la cabine et ouvrir les portes.
+Une personne met exactement 4 dixièmes de seconde pour entrer ou sortir.
+Ceci permet de prédire l’événement FPC en fonction du nombre de personnes qui entrent et sortent.
+*/
+    
     @Override
     public void traiter(Echeancier e, Ascenseur a) {
         //TODO
--- src/MonAscenseur/EvenementPassage.java
+++ src/MonAscenseur/EvenementPassage.java
@@ -1,11 +1,5 @@
 package MonAscenseur;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Maxime Blaise
@@ -29,7 +23,12 @@ public class EvenementPassage extends Evenement {
         this.etageSuivant = etageSuivant;
     }
     
-    
+/*
+Se produit au moment précis où les portes de la cabine viennent juste de terminer leur fermeture.
+Sachant également que le temps mis par la cabine pour aller d’un étage à un autre est constant et vaut exactement 14 dixièmes de seconde,
+l’algorithme de traitement de FPC ajoute le prochain PPC à la bonne date.
+Attention au cas particulier ou l’immeuble serait vide (rare mais possible !).
+*/
 
     @Override
     public void traiter(Echeancier e, Ascenseur a) {
