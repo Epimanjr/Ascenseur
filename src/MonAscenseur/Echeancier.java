c761c5e79e90ab4b814b03e9eb1f4eecdf3c2f63
Tixinoo <antoine.nosal@gmail.com>
2014-03-19 21:34:21 +0100

    // Tixinoo

--- src/MonAscenseur/Echeancier.java
+++ src/MonAscenseur/Echeancier.java
@@ -3,76 +3,81 @@ package MonAscenseur;
 import java.util.*;
 
 /**
- * 
+ *
  * @author Maxime Blaise
  * @author Antoine Nosal
  */
 public class Echeancier {
-    
+
     /**
      * Représente la liste des événements à traiter.
      */
     private final LinkedList listeEvenements;
-    
+
     /**
-     *  Construction de l'échéancier 
+     * Construction de l'échéancier
      */
-    public Echeancier () {
+    public Echeancier() {
         //Initialisation
-	listeEvenements = new LinkedList();
+        listeEvenements = new LinkedList();
     }
-    
+
     /**
      * Ajoute un événement à la liste.
-     * 
-     * @param e 
+     *
+     * @param e
      */
-    public void ajouter (Evenement e) {
-	int pos = 0;
-	while ( pos < listeEvenements.size() ) {
+    public void ajouter(Evenement e) {
+        int pos = 0;
+        boolean ajoute = false;
+        while (pos < listeEvenements.size()) {
             //System.out.println("coucou");
-	    if (((Evenement) listeEvenements.get(pos)).date > e.date) {
-		listeEvenements.add(pos, e);
+            if (((Evenement) listeEvenements.get(pos)).date > e.date) {
+                listeEvenements.add(pos, e);
+                ajoute = true;
                 System.out.println("kiki");
                 break;
-	    } else {
-		pos++;
+            } else {
+                pos++;
                 System.out.println("coco");
-	    }
-	}
-	listeEvenements.add(pos, e);
-    }
-    
-    /**
-     * Récupère le premier énénement pour le traiter.
-     * 
-     * @return 
-     */
-    public Evenement retourneEtEnlevePremier () {
-	Evenement e = (Evenement) listeEvenements.getFirst();
-	listeEvenements.removeFirst();
-	return e;
-    }
-    
-    /**
-     * Retourne vrai si la liste d'événements est vide.
-     * @return 
-     */
-    public boolean estVide() {
-	return (listeEvenements.size() == 0);
-    }
-    
-    /**
-     * Affiche la situation de l'échéancier à un temps t.
-     * 
-     */
-    public void afficheLaSituation() {
-	System.out.print("Echeancier :");
-	int index = 0;
-	while ( index < listeEvenements.size() ) {
-	    ((Evenement) listeEvenements.get(index)).affiche();
-	    index++;
+            }
+        }
+        if (!ajoute) {
+            listeEvenements.add(pos, e);
         }
     }
-    
+
+    /**
+     * Récupère le premier énénement pour le traiter.
+     *
+     * @return
+     */
+    public Evenement retourneEtEnlevePremier() {
+        Evenement e = (Evenement) listeEvenements.getFirst();
+        listeEvenements.removeFirst();
+        return e;
+    }
+
+    /**
+     * Retourne vrai si la liste d'événements est vide.
+     *
+     * @return
+     */
+    public boolean estVide() {
+        return (listeEvenements.size() == 0);
+    }
+
+    /**
+     * Affiche la situation de l'échéancier à un temps t.
+     *
+     */
+    public void afficheLaSituation() {
+        System.out.print("Echeancier :");
+        int index = 0;
+        while (index < listeEvenements.size()) {
+            ((Evenement) listeEvenements.get(index)).affiche();
+            index++;
+        }
+    }
+
 }
